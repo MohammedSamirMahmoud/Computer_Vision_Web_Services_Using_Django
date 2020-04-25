54044a350e308a9243c03980be7e14df2c51b6d5
MohammedSamirMahmoud <mohammed.samir.mahmoud@gmail.com>
2020-04-25 05:34:03 +0200

    edited settings.py - added static dir for loading background images - frontend edit

--- CV_Deployment_Project/CV_Deployment_Project/settings.py
+++ CV_Deployment_Project/CV_Deployment_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App_1_CV_Images',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'CV_Deployment_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'App_1_CV_Images/templates'),os.path.join(BASE_DIR, 'App_2_CV_Videos/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # commented and used default static url for loading background images
